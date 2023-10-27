7edb945ac4a9dcf4eb55fd15a2443bff1b0f16a1
Nithin Babu <nbabu@infoblox.com>
2023-10-27 16:49:57 +0530

    change deprecate item from the code 'domain = vpc'

--- AWS/public_IP.tf
+++ AWS/public_IP.tf
@@ -4,7 +4,7 @@ resource "aws_eip" "pubIP" {
   instance = aws_instance.Test_VM.id
   network_interface = aws_network_interface.foo.id
   associate_with_private_ip = "10.0.1.100"
-  vpc      = true
+  domain   = "vpc"
   tags = {
     Name = "Test_IP"
   }
@@ -14,5 +14,5 @@ resource "aws_eip" "pubIP" {
 #  instance = aws_instance.Test_VM_2.id
 #  network_interface = aws_network_interface.foo.id
 #  associate_with_private_ip = "10.0.1.100"
-#  vpc      = true
+#  domain   = "vpc"
 #}
